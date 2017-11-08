cd54d881caa0fc73a55c4b3ae37ca83ebe94b30f
Raleigh Felton <raleighfelton@gmail.com>
2017-11-07 23:05:12 -0500

    Set Up of Game

---
+++ Chapter 2/Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Raleigh Felton on 11/7/17.
+//  Copyright © 2017 Raleigh Felton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
